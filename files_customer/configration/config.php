795077a0f091038eb117c51d16c0f79f5e8a2c12
AbdAllrhman-odeh <aboodwadi80@gmail.com>
2024-08-21 17:17:47 +0300

    starting with the login page

--- files_customer/configration/config.php
+++ files_customer/configration/config.php
@@ -1,8 +1,50 @@
 <?php
 
-    class db
+    class dataBase
     {
-        
+        private $hostName;
+        private $dbName;
+        private $userName;
+        private $password;
+        private $conn; 
+
+        public function __construct()
+        {
+            $this->hostName='localhost';
+            $this->dbName='ecommerce';
+            $this->userName='root';
+            $this->password='';
+
+            $db="mysql:host=".$this->hostName.";dbname=".$this->dbName;
+
+            try
+            {
+                $this->conn=new PDO($db,$this->userName,$this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+                // echo("done...");
+            }
+            catch(PDOException $e)
+            {
+                echo('Error due to :'.$e->getMessage());
+                echo('<br>');
+                exit('An error occurred. Please try again later.');
+            }
+        }
+
+        public function getConnetion()
+        {
+            return $this->conn;
+        }
+
+        public function __destruct()
+        {
+            $db=null;
+            // echo('End of the connection...');
+            // echo('<br>');
+        }
     }
+
+    // $obj=new dataBase();
 
 ?>
